e4ced966ab87d0f9476df95e8a7e701b8fae2776
Pellikka Minna <h01340@haaga-helia.fi>
2024-11-18 08:59:57 +0200

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import './index.css';
+/* import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -38,8 +38,38 @@ const router = createBrowserRouter([  // Import components that are used in rout
  * logic to your React app, enabling it to handle navigation, 
  * URL changes, and rendering of the defined components for each route.
  */
+/**
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+  </React.StrictMode>
+); */
+
+
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Todos from './components/Todos';
+import Error from './components/Error';
+
+// HashRouter automatically handles URL with hashes (#), so there's no need to manually include the hash in the path.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        {/* The main route "/" will render the App component */}
+        <Route path="/" element={<App />} errorElement={<Error />}>
+          {/* Nested routes */}
+          <Route index element={<Home />} /> {/* Default route */}
+          <Route path="todos" element={<Todos />} />
+          <Route path="kontakti" element={<Contact />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
