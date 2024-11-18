dd5c246803f39bbc492fc80acc8479bc4ab30fb2
Pellikka Minna <h01340@haaga-helia.fi>
2024-11-18 08:55:50 +0200

    deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/{s24todo}/',
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
-});
+})
